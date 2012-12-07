86e4d7a877effa343bbe7827eb5aafc36cee16f3
Piotr Jakubowski <piotrj@gmail.com>
2012-12-07 15:57:53 +0100

    New Command Parser

--- command_parser.rb
+++ command_parser.rb
@@ -3,7 +3,7 @@ require 'json'
 
 URL = 'http://10.0.0.150/command'
 
-class Command < Struct.new(:type)
+class Command
   TYPES = {
     stop: 1,
     forward: 2,
@@ -12,15 +12,25 @@ class Command < Struct.new(:type)
     right: 5
   }
 
+  def initializer(type, power)
+    @type = type
+    @power = power.to_i || 0
+  end
+
   def emit
-    `./emit.sh #{TYPES[type.to_sym]}`
+    puts "command: #{type}, power: #{power}, code: #{code}"
+    `./emit.sh #{code}`
+  end
+
+  def code
+    TYPES[type.to_sym] * 10 + power
   end
 end
 
 while true do
   response = HTTParty.get(URL)
   json = JSON.parse(response.body)
-  command = Command.new(json['command_type'])
+  command = Command.new(json['command_type'], json['power'])
   command.emit
   sleep(1)
 end
